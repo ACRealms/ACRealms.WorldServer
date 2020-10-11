50f144caa8a9f12e2fad42ddd3dfb84726f15e23
Randall Flagg <russellfannin0@gmail.com>
2020-10-10 18:18:22 -0700

    Simplify /zoneinfo output

--- Source/ACE.Server/Realms/AppliedRuleset.cs
+++ Source/ACE.Server/Realms/AppliedRuleset.cs
@@ -44,15 +44,15 @@ namespace ACE.Server.Realms
             sb.AppendLine("AC Realmulator Zone Info:");
 
             foreach (var item in PropertiesBool)
-                sb.AppendLine($"RealmPropertyBool.{Enum.GetName(typeof(RealmPropertyBool), item.Key)} ({(int)item.Key}) = {item.Value}");
+                sb.AppendLine($"{Enum.GetName(typeof(RealmPropertyBool), item.Key)}: {item.Value}");
             foreach (var item in PropertiesFloat)
-                sb.AppendLine($"RealmPropertyFloat.{Enum.GetName(typeof(RealmPropertyFloat), item.Key)} ({(int)item.Key}) = {item.Value}");
+                sb.AppendLine($"{Enum.GetName(typeof(RealmPropertyFloat), item.Key)}: {item.Value}");
             foreach (var item in PropertiesInt)
-                sb.AppendLine($"RealmPropertyInt.{Enum.GetName(typeof(RealmPropertyInt), item.Key)} ({(int)item.Key}) = {item.Value}");
+                sb.AppendLine($"{Enum.GetName(typeof(RealmPropertyInt), item.Key)}: {item.Value}");
             foreach (var item in PropertiesInt64)
-                sb.AppendLine($"RealmPropertyInt64.{Enum.GetName(typeof(RealmPropertyInt64), item.Key)} ({(int)item.Key}) = {item.Value}");
+                sb.AppendLine($"{Enum.GetName(typeof(RealmPropertyInt64), item.Key)}: {item.Value}");
             foreach (var item in PropertiesString)
-                sb.AppendLine($"RealmPropertyString.{Enum.GetName(typeof(RealmPropertyString), item.Key)} ({(int)item.Key}) = {item.Value}");
+                sb.AppendLine($"{Enum.GetName(typeof(RealmPropertyString), item.Key)}: {item.Value}");
 
             sb.AppendLine("\n");
 
