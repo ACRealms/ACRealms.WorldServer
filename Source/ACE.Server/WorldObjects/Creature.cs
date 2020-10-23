c6fac6a120ca804c91e718e6ce249e7250c7d7b7
Randall Flagg <russellfannin0@gmail.com>
2020-10-23 00:26:53 -0700

    fix crash

--- Source/ACE.Server/WorldObjects/Creature.cs
+++ Source/ACE.Server/WorldObjects/Creature.cs
@@ -71,51 +71,72 @@ namespace ACE.Server.WorldObjects
         {
             if (WeenieType == WeenieType.Creature)
             {
-                Biota.PropertiesAttribute2nd[PropertyAttribute2nd.MaxHealth].InitLevel =
-                    (uint)((double)Biota.PropertiesAttribute2nd[PropertyAttribute2nd.MaxHealth].InitLevel *
-                    ruleset.GetProperty(RealmPropertyFloat.CreatureSpawnHPMultiplier));
+                if (Biota?.PropertiesAttribute2nd?.ContainsKey(PropertyAttribute2nd.MaxHealth) == true)
+                {
+                    Biota.PropertiesAttribute2nd[PropertyAttribute2nd.MaxHealth].InitLevel =
+                        (uint)((double)Biota.PropertiesAttribute2nd[PropertyAttribute2nd.MaxHealth].InitLevel *
+                        ruleset.GetProperty(RealmPropertyFloat.CreatureSpawnHPMultiplier));
+                }
 
-                Biota.PropertiesAttribute[PropertyAttribute.Strength].InitLevel =
-                    ClampStat(
-                        (int)Biota.PropertiesAttribute[PropertyAttribute.Strength].InitLevel,
-                        ruleset.GetProperty(RealmPropertyInt.CreatureStrengthAdded),
-                        ruleset.GetProperty(RealmPropertyFloat.CreatureStrengthMultiplier)
-                    );
+                if (Biota?.PropertiesAttribute?.ContainsKey(PropertyAttribute.Strength) == true)
+                {
+                    Biota.PropertiesAttribute[PropertyAttribute.Strength].InitLevel =
+                        ClampStat(
+                            (int)Biota.PropertiesAttribute[PropertyAttribute.Strength].InitLevel,
+                            ruleset.GetProperty(RealmPropertyInt.CreatureStrengthAdded),
+                            ruleset.GetProperty(RealmPropertyFloat.CreatureStrengthMultiplier)
+                        );
+                }
 
-                Biota.PropertiesAttribute[PropertyAttribute.Endurance].InitLevel =
+                if (Biota?.PropertiesAttribute?.ContainsKey(PropertyAttribute.Endurance) == true)
+                {
+                    Biota.PropertiesAttribute[PropertyAttribute.Endurance].InitLevel =
                     ClampStat(
                         (int)Biota.PropertiesAttribute[PropertyAttribute.Endurance].InitLevel,
                         ruleset.GetProperty(RealmPropertyInt.CreatureEnduranceAdded),
                         ruleset.GetProperty(RealmPropertyFloat.CreatureEnduranceMultiplier)
                     );
+                }
 
-                Biota.PropertiesAttribute[PropertyAttribute.Coordination].InitLevel =
+                if (Biota?.PropertiesAttribute?.ContainsKey(PropertyAttribute.Coordination) == true)
+                {
+                    Biota.PropertiesAttribute[PropertyAttribute.Coordination].InitLevel =
                     ClampStat(
                         (int)Biota.PropertiesAttribute[PropertyAttribute.Coordination].InitLevel,
                         ruleset.GetProperty(RealmPropertyInt.CreatureCoordinationAdded),
                         ruleset.GetProperty(RealmPropertyFloat.CreatureCoordinationMultiplier)
                     );
+                }
 
-                Biota.PropertiesAttribute[PropertyAttribute.Quickness].InitLevel =
+                if (Biota?.PropertiesAttribute?.ContainsKey(PropertyAttribute.Quickness) == true)
+                {
+                    Biota.PropertiesAttribute[PropertyAttribute.Quickness].InitLevel =
                     ClampStat(
                         (int)Biota.PropertiesAttribute[PropertyAttribute.Quickness].InitLevel,
                         ruleset.GetProperty(RealmPropertyInt.CreatureQuicknessAdded),
                         ruleset.GetProperty(RealmPropertyFloat.CreatureQuicknessMultiplier)
                     );
+                }
 
-                Biota.PropertiesAttribute[PropertyAttribute.Focus].InitLevel =
+                if (Biota?.PropertiesAttribute?.ContainsKey(PropertyAttribute.Focus) == true)
+                {
+                    Biota.PropertiesAttribute[PropertyAttribute.Focus].InitLevel =
                     ClampStat(
                         (int)Biota.PropertiesAttribute[PropertyAttribute.Focus].InitLevel,
                         ruleset.GetProperty(RealmPropertyInt.CreatureFocusAdded),
                         ruleset.GetProperty(RealmPropertyFloat.CreatureFocusMultiplier)
                     );
+                }
 
-                Biota.PropertiesAttribute[PropertyAttribute.Self].InitLevel =
+                if (Biota?.PropertiesAttribute?.ContainsKey(PropertyAttribute.Self) == true)
+                {
+                    Biota.PropertiesAttribute[PropertyAttribute.Self].InitLevel =
                     ClampStat(
                         (int)Biota.PropertiesAttribute[PropertyAttribute.Self].InitLevel,
                         ruleset.GetProperty(RealmPropertyInt.CreatureSelfAdded),
                         ruleset.GetProperty(RealmPropertyFloat.CreatureSelfMultiplier)
                     );
+                }
             }
         }
 
