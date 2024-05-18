63d8521c73aadd1a237a2330a1dc792f22103f5c
FlaggAC <russellfannin0@gmail.com>
2024-05-18 03:30:37 -0700

    realms.jsonc will now be automatically generated and synced as needed, with an additional layer of validation (but no more need to assign values by hand). It can be thought of as similar to a dependency lockfile for what its worth.

--- Source/ACE.Server/Command/Handlers/ACRealms/RealmDataHelpers.cs
+++ Source/ACE.Server/Command/Handlers/ACRealms/RealmDataHelpers.cs
@@ -1,11 +1,14 @@
 using ACE.Database;
 using ACE.Database.Adapter;
 using ACE.Database.SQLFormatters.World;
+using ACE.Entity.Enum.Properties;
 using ACE.Server.Command.Handlers.Processors;
 using ACE.Server.Managers;
 using ACE.Server.Network;
 using log4net;
+using Newtonsoft.Json;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -34,28 +37,154 @@ namespace ACE.Server.Command.Handlers
             return list;
         }
 
+        public static bool VerifyAndMergeRealmIndex(ISession session, Dictionary<string, ushort> priorLockedIndex, List<RealmToImport> realmsToImport, out Dictionary<string, ushort> newLockedIndex)
+        {
+            var dbRealms = DatabaseManager.World.GetAllRealms(cacheUsage: false).ToDictionary(x => x.Name, x => x.Id);
+            var priorIndexById = priorLockedIndex.ToDictionary(x => x.Value, x => x.Key);
+            var dbRealmsById = dbRealms.ToDictionary(x => x.Value, x => x.Key);
+            newLockedIndex = new Dictionary<string, ushort>(StringComparer.InvariantCultureIgnoreCase);
+
+            bool changesDetected = false;
+
+            foreach(var dbRealmName in dbRealms.Keys)
+            {
+                var dbRealmId = dbRealms[dbRealmName];
+
+                if (!priorLockedIndex.ContainsKey(dbRealmName))
+                {
+                    if (Enum.TryParse<ReservedRealm>(dbRealmName, true, out var reservedRealm))
+                        continue;
+
+                    if (priorIndexById.ContainsKey(dbRealmId))
+                    {
+                        var takenRealmName = priorIndexById[dbRealmId];
+                        throw new InvalidDataException($"Index file defines realm {takenRealmName} with id {dbRealmId} but but the a realm exists in the database with the same ID under the name {dbRealmName}");
+                    }
+
+                    CommandHandlerHelper.WriteOutputInfo(session, $"Syncing realm {dbRealmName} to index file.");
+                    changesDetected = true;
+                    newLockedIndex.Add(dbRealmName, dbRealmId);
+                    continue;
+                }
+                else if (Enum.TryParse<ReservedRealm>(dbRealmName, true, out var reservedRealm))
+                    throw new InvalidDataException($"A reserved realm exists with the name {reservedRealm} and may not be used as a realm name.");
+
+                var priorRealmId = priorLockedIndex[dbRealmName];
+
+                if (priorRealmId != dbRealmId)
+                    throw new InvalidDataException($"Realm or Ruleset {dbRealmName} exists in DB with ID {dbRealmId} but defined in index file with id {priorRealmId}");
+                else
+                    newLockedIndex.Add(dbRealmName, priorLockedIndex[dbRealmName]);
+            }
+
+            foreach (var priorRealmName in priorLockedIndex.Keys)
+            {
+                if (newLockedIndex.ContainsKey(priorRealmName))
+                    continue;
+
+                var priorRealmId = priorLockedIndex[priorRealmName];
+
+                if (dbRealmsById.ContainsKey(priorRealmId))
+                    throw new InvalidDataException($"Realm or Ruleset {priorRealmName} exists in DB with ID {dbRealms[priorRealmName]} but defined in index file with id {priorRealmId}");
+                else if (dbRealms.ContainsKey(priorRealmName))
+                    throw new InvalidDataException($"Realm or Ruleset {priorRealmName} defined in index file with ID {priorRealmId} but defined in DB with id {dbRealms[priorRealmName]}");
+
+                changesDetected = true;
+                CommandHandlerHelper.WriteOutputInfo(session, $"Found new realm {priorRealmName} in index file with self-assigned ID of {priorRealmId}.");
+                newLockedIndex.Add(priorRealmName, priorRealmId);
+            }
+
+            var reservedIds = newLockedIndex.Select(x => x.Value).ToHashSet();
+
+            var reservedRealmIdsByEnum = Enum.GetValues<ReservedRealm>();
+            foreach(var enumVal in reservedRealmIdsByEnum)
+            {
+                if (reservedIds.Contains((ushort)enumVal) && !dbRealmsById.ContainsKey((ushort)enumVal))
+                    throw new InvalidDataException($"The ID {(ushort)enumVal} is not permitted to be manually added to the index file because there is a system-defined realm {enumVal} with that ID.");
+                reservedIds.Add((ushort)enumVal);
+            }
+
+            ushort nextId = 0;
+            var findNextId = () => {
+                while (reservedIds.Contains(++nextId)) { }
+                
+                return nextId;
+            };
+            
+
+            foreach(var realm in realmsToImport)
+            {
+                var name = realm.Realm.Name;
+                if (newLockedIndex.ContainsKey(name))
+                    continue;
+                changesDetected = true;
+                newLockedIndex.Add(name, findNextId());
+                reservedIds.Add(newLockedIndex[name]);
+            }
+
+
+            // Be absolutely sure
+            if (!changesDetected)
+            {
+                if (!priorLockedIndex.OrderBy(kvp => kvp.Key).SequenceEqual(newLockedIndex.OrderBy(kvp => kvp.Key)))
+                    throw new InvalidOperationException("Internal error when attempting to sync the index file.");
+            }
+            else
+            {
+                if (priorLockedIndex.OrderBy(kvp => kvp.Key).SequenceEqual(newLockedIndex.OrderBy(kvp => kvp.Key)))
+                    throw new InvalidOperationException("Internal error when attempting to sync the index file.");
+            }
+
+            return changesDetected;
+        }
+
         public static void ImportJsonRealmsIndex(ISession session, string realmsIndexJsonFile, List<RealmToImport> realms)
         {
             Dictionary<string, RealmToImport> realmsDict = null;
-            Dictionary<ushort, RealmToImport> realmsById = new Dictionary<ushort, RealmToImport>();
             try
             {
                 realmsDict = realms.ToDictionary(x => x.Realm.Name);
             }
-            catch
+            catch(Exception ex)
             {
-                CommandHandlerHelper.WriteOutputInfo(session, $"Couldn't create realms dictionary. Is there a problem with a realm name?");
+                log.Error(ex);
+                CommandHandlerHelper.WriteOutputError(session, $"Couldn't create realms dictionary. Is there a problem with a realm name?");
                 return;
             }
 
-            Dictionary<string, ushort> result = null;
+            Dictionary<string, ushort> realmsIndex = null;
+            if (!File.Exists(realmsIndexJsonFile))
+            {
+                realmsIndex = new Dictionary<string, ushort>();
+            }
+            else
+            {
+                try
+                {
+                    var fileText = File.ReadAllText(realmsIndexJsonFile);
+                    realmsIndex = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, ushort>>(fileText);
+                }
+                catch (Exception ex)
+                {
+                    log.Error(ex);
+                    CommandHandlerHelper.WriteOutputError(session, $"Failed to interpret contents of {realmsIndexJsonFile}.");
+                    return;
+                }
+            }
+
+            if (VerifyAndMergeRealmIndex(session, realmsIndex, realms, out var newIndex))
+            {
+                var newIndexJson = JsonConvert.SerializeObject(newIndex, Formatting.Indented) + Environment.NewLine;
+                File.WriteAllText(realmsIndexJsonFile, newIndexJson);
+                CommandHandlerHelper.WriteOutputInfo(session, $"Changes were made to index lockfile {realmsIndexJsonFile}, please commit these changes to your git repository.");
+            }
+            realmsIndex = newIndex;
+
+            Dictionary<ushort, RealmToImport> realmsById = new Dictionary<ushort, RealmToImport>();
             try
             {
-                var fileText = File.ReadAllText(realmsIndexJsonFile);
-                result = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, ushort>>(fileText);
-
                 //Map ids
-                foreach (var item in result)
+                foreach (var item in realmsIndex)
                 {
                     var importItem = realmsDict[item.Key];
                     importItem.Realm.SetId(item.Value);
@@ -63,7 +192,7 @@ namespace ACE.Server.Command.Handlers
                 }
 
                 //Map parents
-                foreach (var item in result)
+                foreach (var item in realmsIndex)
                 {
                     var importItem = realmsDict[item.Key];
                     if (importItem.Realm.ParentRealmName != null)
@@ -75,7 +204,7 @@ namespace ACE.Server.Command.Handlers
                 }
 
                 //Map descendents
-                foreach (var item in result)
+                foreach (var item in realmsIndex)
                 {
                     var importItem = realmsDict[item.Key];
                     if (importItem.Realm.ParentRealmId == null)
