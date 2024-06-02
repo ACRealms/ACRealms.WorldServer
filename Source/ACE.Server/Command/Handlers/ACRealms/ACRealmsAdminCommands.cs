7a4b5522c12843d610021460baa4fb9ac5bbe2cc
FlaggAC <russellfannin0@gmail.com>
2024-06-02 00:13:18 -0700

    Add get-home-realm Admin command

--- Source/ACE.Server/Command/Handlers/ACRealms/ACRealmsAdminCommands.cs
+++ Source/ACE.Server/Command/Handlers/ACRealms/ACRealmsAdminCommands.cs
@@ -1,5 +1,6 @@
 using ACE.Database;
 using ACE.Entity.Enum;
+using ACE.Entity.Enum.Properties;
 using ACE.Server.Managers;
 using ACE.Server.Network;
 using ACE.Server.Realms;
@@ -13,6 +14,63 @@ namespace ACE.Server.Command.Handlers.ACRealms
 {
     public class ACRealmsAdminCommands
     {
+        [CommandHandler("get-home-realm", AccessLevel.Admin, CommandHandlerFlag.None, 1,
+            "Get a home realm for a character.",
+            "Character name")]
+        public static void HandleGetHomeRealm(ISession session, params string[] parameters)
+        {
+            var name = string.Join(" ", parameters);
+
+            var characterName = name.TrimStart(' ').TrimEnd(' ');
+            if (characterName.StartsWith("+"))
+                characterName = characterName.Substring(1);
+
+            var onlinePlayer = PlayerManager.GetOnlinePlayer(characterName);
+            var offlinePlayer = PlayerManager.GetOfflinePlayer(characterName);
+
+            int? homeRealmRawId;
+            if (onlinePlayer != null)
+                homeRealmRawId = onlinePlayer.GetProperty(PropertyInt.HomeRealm);
+            else if (offlinePlayer != null)
+                homeRealmRawId = offlinePlayer.GetProperty(PropertyInt.HomeRealm);
+            else
+            {
+                CommandHandlerHelper.WriteOutputInfo(session, $"Character not found: '{characterName}'", ChatMessageType.Broadcast);
+                return;
+            }
+
+            var prefix = $"Home Realm for '{characterName}':";
+
+            if (homeRealmRawId == null)
+            {
+                CommandHandlerHelper.WriteOutputInfo(session, $"{prefix} [not present]", ChatMessageType.Broadcast);
+                return;
+            }
+            if (homeRealmRawId.Value < 0 || homeRealmRawId > 0x7FFF)
+            {
+                CommandHandlerHelper.WriteOutputInfo(session, $"{prefix} {homeRealmRawId} (invalid, out of range)", ChatMessageType.Broadcast);
+                return;
+            }
+            var homeRealmId = (ushort)homeRealmRawId;
+
+            if (homeRealmId == 0)
+            {
+                CommandHandlerHelper.WriteOutputInfo(session, $"{prefix} {homeRealmId} (invalid, NULL realm not allowed)", ChatMessageType.Broadcast);
+                return;
+            }
+
+            var realm = RealmManager.GetRealm(homeRealmId, includeRulesets: false);
+            if (realm == null)
+            {
+                CommandHandlerHelper.WriteOutputInfo(session, $"{prefix} {homeRealmId} (invalid, valid realm not found)", ChatMessageType.Broadcast);
+                return;
+            }
+
+            CommandHandlerHelper.WriteOutputInfo(session, $"{prefix} {homeRealmId} ({realm.Realm.Name})", ChatMessageType.Broadcast);
+            return;
+        }
+
+
         [CommandHandler("set-home-realm", AccessLevel.Admin, CommandHandlerFlag.None, 2,
             "Set a home realm for a character.",
             "Character name, Realm ID or Realm Name")]
