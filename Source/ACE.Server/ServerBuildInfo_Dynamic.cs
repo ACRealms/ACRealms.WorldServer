846a8054201bc72163550d4a0d8077e6f0ad2409
FlaggAC <russellfannin0@gmail.com>
2024-06-01 22:42:18 -0700

    Bump version to 2.1.0-beta2

--- Source/ACE.Server/ServerBuildInfo_Dynamic.cs
+++ Source/ACE.Server/ServerBuildInfo_Dynamic.cs
@@ -6,7 +6,7 @@ namespace ACE.Server
         public static string Branch = "master"; 
         public static string Commit = "bbf0afdaa7a2a6756d8dd84434e636c67b1f4e95"; 
  
-        public static string Version = "2.1.0-preview"; 
+        public static string Version = "2.1.0-beta2"; 
         public static string Build   = "0"; 
  
         public static int BuildYear   = 2024; 
