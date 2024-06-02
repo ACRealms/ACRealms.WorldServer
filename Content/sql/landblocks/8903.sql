6a0d8ad63433834ecb3b318e63b717c6b949b5da
FlaggAC <russellfannin0@gmail.com>
2024-06-01 21:49:00 -0700

    Remove landblock content. Added an optional folder for duel realm landblock content

--- Content/sql/landblocks/8903.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM `landblock_instance` WHERE `landblock` = 0x8903;
-
-INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
-VALUES (0x78903000, 4200002, 0x8903012E, 90.2252, -50.3203, 0.005, -0.932963, 0, 0, -0.359971, False, '2020-10-18 06:14:12'); /* Blaine */
-/* @teleloc 0x8903012E [90.225197 -50.320301 0.005000] -0.932963 0.000000 0.000000 -0.359971 */
