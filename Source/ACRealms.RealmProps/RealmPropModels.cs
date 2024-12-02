b908d56428257d4ce23fcbed8af3e0457dc18ff6
FlaggAC <russellfannin0@gmail.com>
2024-12-02 06:48:56 -0800

    wip

--- Source/ACRealms.RealmProps/RealmPropModels.cs
+++ Source/ACRealms.RealmProps/RealmPropModels.cs
@@ -7,7 +7,9 @@ namespace ACRealms.RealmProps
     // https://github.com/corvus-dotnet/corvus.jsonschema?tab=readme-ov-file#getting-started
     // dotnet tool install --global Corvus.Json.JsonSchema.TypeGeneratorTool
     // generatejsonschematypes ACE.Entity\ACRealms\RealmProps\json-schema\realm-property-schema.json
-
+    /* 
+     generatejsonschematypes ACE.Entity\ACRealms\RealmProps\json-schema\realm-property-schema.json --rootNamespace ACRealms.RealmProps.IntermediateModels --outputPath= ACRealms.RealmProps\RealmPropModels
+    */
     // dotnet tool install --global Corvus.Json.JsonSchema.TypeGeneratorTool --version 3.1.1
     internal abstract record RealmPropModel { }
 
